4dacad7c34d7a27c53996cdd3bf390fb2d595e73
Julio Rodriguez <julioalfonso8003@gmail.com>
2021-11-19 18:51:02 -0500

    Warning..

--- GolangCodeAvanzed/net/port.go
+++ GolangCodeAvanzed/net/port.go
@@ -9,6 +9,7 @@ func main() {
 	for i := 0; i < 100; i++ {
 		// 1, 2, ...99
 		//1-> Open or closed
+		//Solo sitios de pruebas, puede ser considerado un ataque.
 		conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", "scanme.nmap.org", i))
 		if err != nil {
 			continue
