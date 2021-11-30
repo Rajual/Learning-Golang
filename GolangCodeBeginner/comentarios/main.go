663eb45da38f3e22251b244fae39ef57902d46d2
Julio Rodriguez <julioalfonso8003@gmail.com>
2021-11-30 18:06:58 -0500

    Learning golang

---
+++ GolangCodeBeginner/comentarios/main.go
@@ -0,0 +1,17 @@
+package main
+
+//Comentario de una linea (Para todo lo que no sea codigo)
+
+// Pi es una constante
+const Pi = 3.14
+
+/*
+Comentario multilinia (Para codigo)
+*/
+/*
+func main()  {
+
+}
+*/
+
+//run: go doc --all
