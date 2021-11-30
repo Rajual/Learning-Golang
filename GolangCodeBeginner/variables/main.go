9fe1ad103696c31123bce4fe9d3b25a21f90c171
Julio Rodriguez <julioalfonso8003@gmail.com>
2021-11-30 18:08:03 -0500

    Learning golang

---
+++ GolangCodeBeginner/variables/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var cat string="cat"
+	//var cat ="cat"
+	dog, cat := ":dog", ":cat"
+	cat, face := "gato", ":face"
+	fmt.Println(dog, cat, face)
+}
