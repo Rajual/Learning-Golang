79f20a7b985f404fe9b1f590f8406ed9b2228687
Julio Rodriguez <julioalfonso8003@gmail.com>
2021-12-02 16:28:11 -0500

    array

---
+++ GolangCodeBeginner/array/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*var food [3]string
+	food[0] = ":burger"
+	food[1] = ":pizza"
+	food[2] = ":hotdog"*/
+
+	food := [3]string{":burger", ":pizza", ":hotdog"}
+
+	fmt.Println(food)
+}
