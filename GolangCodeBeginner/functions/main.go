4def9a03c5d9346cc4539c95c0bc86bb7a4b2d64
Rajual <julioalfonso8003@gmail.com>
2021-12-08 15:27:46 -0500

    functions

--- GolangCodeBeginner/functions/main.go
+++ GolangCodeBeginner/functions/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"strings"
 )
 
@@ -20,6 +22,74 @@ func main() {
 	//Retorno de multiples valores
 	fmt.Println(convert("Julio"))
 
+	//Controlar errores
+	content, err := ioutil.ReadFile("./things.txt")
+	if err == nil {
+		fmt.Println(err)
+		return
+	}
+	//Retorna pues esta el error
+	/* else {
+		fmt.Println(string(content))
+	}*/
+	fmt.Println(string(content))
+	resultado, err := division(10, 2)
+	if err != nil {
+		return
+	}
+	fmt.Println(resultado)
+
+	//Funciones que reciben y retornan funciones
+	//Se recibe una funcion como parametro
+	nums := []int{1, 4, 70, 5, 67, 90, 2}
+	filterNumber := filter(nums, func(i int) bool {
+		if i <= 10 {
+			return true
+		}
+		return false
+	})
+	fmt.Println(filterNumber)
+	//Se retorna una funcion
+	fmt.Println(hello("Julio")("¿Como estas?"))
+
+	//Funciones variaticas
+	fmt.Println(suma(1, 2, 3, 4))
+
+	//Funciones anonimas
+	f := func() {
+		fmt.Println("Hello Julio!")
+	}
+	f()
+	//Puntero a la funcion
+	fmt.Println(f)
+}
+
+//Funciones variatica
+func suma(nums ...int) int {
+	sum := 0
+	for _, v := range nums {
+		sum += v
+	}
+	return sum
+}
+
+//Funcioes que retornan una funcion
+func hello(name string) func(message string) string {
+	return func(m string) string {
+		return "Hello " + name + " " + m
+	}
+}
+
+//Funciones que reciben funciones
+func filter(nums []int, callback func(int) bool) []int {
+	result := []int{}
+	for _, v := range nums {
+		if callback(v) {
+			result = append(result, v)
+		}
+
+	}
+	return result
 }
 
 func convert(text string) (string, string) {
@@ -37,4 +107,14 @@ func helloWork(name string, lastName string) {
 
 func change(name *string) {
 	*name = "Alfonso"
+}
+
+//Parametros nombrado
+func division(dividendo, divisor int) (result int, err error) {
+	if divisor == 0 {
+		err = errors.New("No puedes divider entre 0")
+		return
+	}
+	result = dividendo / divisor
+	return
 }
